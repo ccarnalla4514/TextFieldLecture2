fae2794438c0d2b1b70e827b131aa5b0d608e304
Christian Carnalla <ccarnalla4514@stu.d214.org>
2020-10-28 14:56:29 -0500

    Initial Commit

---
+++ TextFieldLecture2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextFieldLecture2
+//
+//  Created by Christian Carnalla on 10/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
